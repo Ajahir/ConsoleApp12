9e4cfea6d5f25782024b56d0da68463981b0e5df
aeris.ahir <aeris.ahir@1R-75>
2022-02-15 17:51:21 +0530

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        private static TvChannels tvC;
+        static void Main(string[] args)
+        {
+            tvC = new TvChannels(3);
+            tvC.GetSeriesDetails();
+        }
+    }
+    interface ISeries
+    {
+        void LoadSeriesDetails();
+    }
+    class TvChannels
+    {
+
+
+        private ISeries series = null;
+        SeriesLauncher sl = new SeriesLauncher();
+
+        public TvChannels(int index)
+        {
+            this.series = sl.LaunchEvent(index);
+        }
+
+        //private ISeries Series;
+        /* public ISeries Myseries
+         {
+             set 
+             {
+                 Series = value;
+             }
+         }*/
+
+        public void GetSeriesDetails()
+        {
+
+            this.series.LoadSeriesDetails();
+
+        }
+
+
+    }
+
+    class SeriesLauncher
+    {
+
+        public ISeries LaunchEvent(int index)
+        {
+            if (index == 1)
+                return new CC();
+            else if (index == 2)
+                return new TLC();
+            else
+                return new TravelerXp();
+
+
+        }
+
+    }
+
+    class CC : ISeries
+    {
+
+        public void LoadSeriesDetails()
+        {
+            Console.WriteLine("Welocme to sdk");
+            Console.ReadKey();
+        }
+
+        /* string ISeries.LoadSeriesDetails()
+         {
+             string cc= "Welocme to Comedy Central";
+             return cc;
+         }*/
+    }
+
+    class TLC : ISeries
+    {
+        public void LoadSeriesDetails()
+        {
+            Console.WriteLine("Welocme to jdk");
+            Console.ReadKey();
+        }
+
+        /*string ISeries.LoadSeriesDetails()
+        {
+            string tlc = "Welocme to TLC";
+            return tlc;
+        }*/
+    }
+
+    class TravelerXp : ISeries
+    {
+        public void LoadSeriesDetails()
+        {
+            Console.WriteLine("Welocme to api");
+            Console.ReadKey();
+        }
+
+        /*string ISeries.LoadSeriesDetails()
+        {
+            string txp = "Welocme to Traveler XP";
+            return txp;
+        }*/
+    }
+}
+
